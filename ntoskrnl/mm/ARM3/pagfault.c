3f026d9e8586ce693bdd5a652991e201000e3ae7
Jérôme Gardou <jerome.gardou@reactos.org>
2021-02-11 18:51:00 +0100

    [NTOS:MM] Do not add the page in the WS list when serving a proto-transition fault.
    This will be done later

--- ntoskrnl/mm/ARM3/pagfault.c
+++ ntoskrnl/mm/ARM3/pagfault.c
@@ -1106,8 +1106,8 @@ MiResolveTransitionFault(IN BOOLEAN StoreInstruction,
     /* Write the valid PTE */
     MI_WRITE_VALID_PTE(PointerPte, TempPte);
 
-    /* If this was a user fault, add it to the working set */
-    if (CurrentProcess > HYDRA_PROCESS)
+    /* If this was a user fault (no prototype), add it to the working set */
+    if ((CurrentProcess > HYDRA_PROCESS) && (PointerPte == MiAddressToPte(FaultingAddress)))
         MiInsertInWorkingSetList(&CurrentProcess->Vm, FaultingAddress, Protection);
 
     /* Return success */
