c50bbc45b48a1831b1aae332f873b3d15b60da7c
Jérôme Gardou <jerome.gardou@reactos.org>
2021-03-03 13:29:13 +0100

    [NTOS:MM] Drop a shared reference on the right page when deleting a transition PTE

--- ntoskrnl/mm/ARM3/virtual.c
+++ ntoskrnl/mm/ARM3/virtual.c
@@ -428,7 +428,9 @@ MiDeletePte(
             MI_ERASE_PTE(PointerPte);
 
             /* Drop the reference on the page table. */
-            MiDecrementShareCount(MiGetPfnEntry(Pfn1->u4.PteFrame), Pfn1->u4.PteFrame);
+            PointerPde = MiPteToPde(PointerPte);
+            MiDecrementShareCount(MiGetPfnEntry(PointerPde->u.Hard.PageFrameNumber),
+                PointerPde->u.Hard.PageFrameNumber);
 
             ASSERT(Pfn1->u3.e1.PrototypePte == 0);
 
@@ -536,6 +538,7 @@ MiDeletePte(
         ASSERT(Pfn1->u2.ShareCount == 1);
 
         /* Drop the reference on the page table. */
+        ASSERT(Pfn1->u4.PteFrame == MiPteToPde(PointerPte)->u.Hard.PageFrameNumber);
         MiDecrementShareCount(MiGetPfnEntry(Pfn1->u4.PteFrame), Pfn1->u4.PteFrame);
 
         /* Mark the PFN for deletion and dereference what should be the last ref */
