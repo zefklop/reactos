5e5b238976da7db5ed7e6f02802053a4f0818c6f
Jérôme Gardou <jerome.gardou@reactos.org>
2021-03-10 15:24:31 +0100

    [NTOS:MM] Get rid of MmRosFlushVirtualMemory
    It's not used anywhere now, and it will eventually be gone

--- ntoskrnl/mm/ARM3/virtual.c
+++ ntoskrnl/mm/ARM3/virtual.c
@@ -1374,8 +1374,10 @@ MmFlushVirtualMemory(IN PEPROCESS Process,
                      OUT PIO_STATUS_BLOCK IoStatusBlock)
 {
     PAGED_CODE();
-    /* For now we call the old Mm */
-    return MmRosFlushVirtualMemory(Process, BaseAddress, RegionSize, IoStatusBlock);
+
+    UNIMPLEMENTED;
+
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 ULONG
