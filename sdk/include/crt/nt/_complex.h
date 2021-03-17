775e90e5cd30702f4671360250592e20e50933f3
Jérôme Gardou <jerome.gardou@reactos.org>
2021-03-17 09:16:07 +0100

    [CRT] Split complex.h header

---
+++ sdk/include/crt/nt/_complex.h
@@ -0,0 +1,7 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ */
+
+#pragma once
+
+#error "Complex numbers support is not implemented in this CRT implementation"
