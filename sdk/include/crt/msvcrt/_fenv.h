20b4dcfc3a82e6787a30848b014f66e01c1f4599
Jérôme Gardou <jerome.gardou@reactos.org>
2021-03-17 09:53:21 +0100

    [CRT] Split fenv.h header
    Also don't pretend we implement stuff in there

---
+++ sdk/include/crt/msvcrt/_fenv.h
@@ -0,0 +1,15 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#error "fenv.h is not implemented in ReactOS CRT"
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
---
+++ sdk/include/crt/nt/_fenv.h
@@ -0,0 +1,7 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ */
+
+#pragma once
+
+#error "FPU environment not implemented in this CRT implementation"
